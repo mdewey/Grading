4cd26545414e56a5f5037191b4e272f8cce3f2c4
Mark Dewey <mark.t.dewey@gmail.com>
2017-01-05 16:16:35 -0500

    added the first commit working project

---
+++ GradingWizard/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PowerShell shell = PowerShell.Create())
+            {
+                shell.AddCommand("Write-Host 'Hello World'");
+                shell.Invoke();
+            }
+        }
+    }
+}
---
+++ grading.ps1
@@ -0,0 +1,30 @@
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$true)]
+  [string]$username,
+    [Parameter(Mandatory=$true)]
+  [string]$repo
+)
+
+Write-Host "Starting the clone!"
+
+
+#$url = "https://github.com/SherryHall/Weatherman.git";
+#$username = "SherryHall";
+#$username = "wuashiboy";
+$url  = "https://github.com/" + $username + "/" + $repo + ".git";
+
+$reset = "rm -Force -r " + $repo;
+#iex $reset;
+
+$clone = "git clone " + $url ;
+iex $clone;
+
+$move = "cd " + $repo;
+iex $move;
+
+$file  = Get-ChildItem *.sln -Recurse;
+$start = "start " + $file;
+iex $start;
+
+cd ..
